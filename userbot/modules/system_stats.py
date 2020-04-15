5a4131f3f14611e49875f4c1fd95aa956f368f6b
Shéikh Adnan <manofuranium@gmail.com>
2020-04-15 20:26:03 +0530

    .alive update

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -122,13 +122,13 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`My Detail Ubot `\n"
+    await alive.edit("`My Ubot's details `\n"
                      f"> `Telethon : v{version.__version__} `\n"
                      f"> `Python : v{python_version()} `\n"
 	                 "===================== \n"
                      f"`User : `{DEFAULTUSER} \n"
-		             "===================== \n")
-
+		             "===================== \n"
+                     f"`#DirtyBlack_EXTENDED`\n")
 
 @register(outgoing=True, pattern="^.aliveu")
 async def amireallyaliveuser(username):
