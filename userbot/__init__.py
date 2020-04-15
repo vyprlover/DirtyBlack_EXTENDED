05bc830ed5199695324c60dc0d127b1e0c873f17
Shéikh Adnan <manofuranium@gmail.com>
2020-04-15 18:31:21 +0530

    Updater patched

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -79,7 +79,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL and BRANCH for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/MoveAngel/One4uBot.git")
+    "github.com/ElytrA8/DirtyBlack_EXTENDED.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "sql-extended")
 
