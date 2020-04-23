99fadaecc5d6e3eede2ddc1fb73555da9e76ff13
Shéikh Adnan <manofuranium@gmail.com>
2020-04-23 13:57:25 +0530

    Add files via upload

---
+++ userbot/modules/Earth.py
@@ -0,0 +1,19 @@
+from userbot import CMD_HELP
+from userbot.events import register
+
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@register(events.NewMessage(pattern=r"\.earth", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
+	for _ in range(48):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+		CMD_HELP.update({ "earth":
+			"earth emoji"})
