677f601119db5575468f0f3f6b8c04d0041596a1
Shéikh Adnan <manofuranium@gmail.com>
2020-04-24 05:57:18 +0530

    Update webupload.py

--- userbot/modules/webupload.py
+++ userbot/modules/webupload.py
@@ -22,7 +22,7 @@ async def _(event):
         reply = await event.get_reply_message()
         file_name = await bot.download_media(reply.media, Var.TEMP_DOWNLOAD_DIRECTORY)
     reply_to_id = event.message.id
-    CMD_WEB = {"anonfiles": "curl -F \"file=@{}\" https://anonfiles.com/api/upload", "transfer": "curl --upload-file \"{}\" https://transfer.sh/{os.path.basename(file_name)}", "filebin": "curl -X POST --data-binary \"@test.png\" -H \"filename: {}\" \"https://filebin.net\"", "anonymousfiles": "curl -F file=\"@{}\" https://api.anonymousfiles.io/", "megaupload": "curl -F \"file=@{}\" https://megaupload.is/api/upload", "bayfiles": "curl -F \"file=@{}\" https://bayfiles.com/api/upload, "openload": "curl -F \"file=@{}\" https://api.openload.cc/upload"}
+    CMD_WEB = {"anonfiles": "curl -F \"file=@{}\" https://anonfiles.com/api/upload", "transfer": "curl --upload-file \"{}\" https://transfer.sh/{os.path.basename(file_name)}", "filebin": "curl -X POST --data-binary \"@test.png\" -H \"filename: {}\" \"https://filebin.net\"", "anonymousfiles": "curl -F file=\"@{}\" https://api.anonymousfiles.io/", "megaupload": "curl -F \"file=@{}\" https://megaupload.is/api/upload", "bayfiles": "curl -F \"file=@{}\" https://bayfiles.com/api/upload, "openload": "curl -F \"file=@{}\" https://api.openload.cc/upload" }
     try:
         selected_one = CMD_WEB[selected_transfer].format(file_name)
     except KeyError:
@@ -35,4 +35,4 @@ async def _(event):
     stdout, stderr = await process.communicate()
     await event.edit(f"{stdout.decode()}")
 CMD_HELP.update({ "webupload":
-"`.webupload` (filename) `--anonfiles` | `transfer` | `filebin` | `anonymousfiles` | `megaupload` | `bayfiles` "})  
+"`.webupload` (filename) `--anonfiles` | `transfer` | `filebin` | `anonymousfiles` | `megaupload` | `bayfiles` | `openload` "})  
