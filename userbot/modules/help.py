3ca6f884e35a3e8a7f444ad59acc8ca796bd6a44
Shéikh Adnan <manofuranium@gmail.com>
2020-04-24 20:48:26 +0530

    Trying

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -22,7 +22,7 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t\t\t||\t\t\t "
+            string += "`\t\t\t✓\t\t\t "
         await event.edit(f"{string}"
                         "\n\nSpecify which module do you want help for !!\
                         \n**Usage:** `.help` <module name>")
