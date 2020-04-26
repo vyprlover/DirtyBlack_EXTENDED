124992cd9bb11bb4d310644490fc0cfaa629b3cd
Shéikh Adnan <manofuranium@gmail.com>
2020-04-26 18:30:23 +0530

    Sticke

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot module for kanging stickers or making new ones. Thanks @rupansh"""
+""" Userbot module for kanging stickers or making new ones."""
 
 import io
 import math
@@ -30,6 +30,7 @@ KANGING_STR = [
     "Imprisoning this sticker...",
     "Mr.Steal Your Sticker is stealing this sticker... ",
     "That's a nice sticker that deserves to live in my pack",
+    "你好，我在偷你的贴纸",
 ]
 
 
