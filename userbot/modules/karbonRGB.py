1b7c637d148e7d1bcf83a3e593a06765562ec452
Shéikh Adnan <manofuranium@gmail.com>
2020-05-05 07:45:23 +0530

    Update karbonRGB.py

--- userbot/modules/karbonRGB.py
+++ userbot/modules/karbonRGB.py
@@ -83,7 +83,7 @@ async def carbon_api(e):
    url = CARBON.format(code=code, R=RED, G=GREEN, B=BLUE, T=The, lang=CARBONLANG)
    chrome_options = Options()
    chrome_options.add_argument("--headless")
-   chrome_options.binary_location = Config.CHROME_DRIVER
+   chrome_options.binary_location = GOOGLE_CHROME_BIN
    chrome_options.add_argument("--window-size=1920x1080")
    chrome_options.add_argument("--disable-dev-shm-usage")
    chrome_options.add_argument("--no-sandbox")
