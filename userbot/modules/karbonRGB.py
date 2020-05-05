afd45e395992746a0a4dee7bb7eb96a97c6199f2
Shéikh Adnan <manofuranium@gmail.com>
2020-05-05 08:05:52 +0530

    Update karbonRGB.py

--- userbot/modules/karbonRGB.py
+++ userbot/modules/karbonRGB.py
@@ -90,13 +90,13 @@ async def carbon_api(e):
    chrome_options.add_argument("--disable-dev-shm-usage")
    chrome_options.add_argument("--no-sandbox")
    chrome_options.add_argument('--disable-gpu')
-   prefs = {'download.default_directory' : './'}
+   prefs = {'download.default_directory' : './download/'}
    chrome_options.add_experimental_option('prefs', prefs)
    await e.edit("⬛⬛⬜⬜⬜")
 
    driver = webdriver.Chrome(executable_path= CHROME_DRIVER, options=chrome_options)
    driver.get(url)
-   download_path = './'
+   download_path = './download/'
    driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
    params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
    command_result = driver.execute("send_command", params)
