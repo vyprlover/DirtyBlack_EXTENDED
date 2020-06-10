44783bc69efbae4cec9a507bc853fd35255d261a
Shéikh Adnan <manofuranium@gmail.com>
2020-06-10 13:53:01 +0530

    Tesla ota

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -79,9 +79,9 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL and BRANCH for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/ElytrA8/DirtyBlack_EXTENDED.git")
+    "https://github.com/ElytrA8/TESLA.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "sql-extended")
+    "UPSTREAM_REPO_BRANCH", "TESLA")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
